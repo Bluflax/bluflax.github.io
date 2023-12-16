00d0543a709cd4f04886a5a0689cb8487c04cc8d
Bluflax <121681174+Bluflax@users.noreply.github.com>
2023-12-17 02:59:22 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const btnfloat = document.getElementById('btnfloat');
-btnfloat.addEventListener('click', function() {
-    btnfloat.style.transform = 'scale(0.95)';
-    setTimeout(() => {
-        btnfloat.transform = 'scale(1)';
-    }, 300);
-    window.location.href = 'https://bflstree.com';
-})
