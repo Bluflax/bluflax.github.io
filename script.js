f1f57b2efdde7e761621ddab695f7933257e695d
Bluflax <121681174+Bluflax@users.noreply.github.com>
2024-01-31 14:58:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const Header = document.querySelector(".Header");
     const Block = document.querySelector(".block");
 
-
     setTimeout(function() {
         welcomeText.style.opacity = "1";
         Header.style.transform = "translateY(30px)";
@@ -23,4 +22,19 @@ document.addEventListener("DOMContentLoaded", function() {
         Header.style.transform = "translateY(-0px)";
         Block.style.transform = "translateY(-0px)";
     }, 2200);
+
+    setTimeout(function() {
+        window.addEventListener("scroll", function() {
+            const scroll=window.scrollY > 10;
+            Header.classList.toggle("scrolled", scroll);
+            if (scroll) {
+                Header.style.transform = "translateY(-40px) scale(0.7)"
+            }
+            else {
+                Header.style.transform = "translateY(0px) scale(1)"
+            }
+        });
+    }, 2200);
+
 });
+
