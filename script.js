19bea7e1129f3a727cad4258a4403839773fb3b7
Bluflax <121681174+Bluflax@users.noreply.github.com>
2024-04-01 20:17:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const Box = document.querySelectorAll(".box");
     const Box2 = document.getElementById("box2");
     const Above = document.querySelectorAll(".above");
+    const Ab2 = document.getElementById('above2')
     const Filler = document.querySelector(".filler");
     const Ctop = document.getElementById("ctop");
     const Salert = document.getElementById('sensitivemobile');
-    const Access = document.getElementById('access');
-    const Notice = document.getElementById('notice1');
 
     const toady = new Date();
     const GK = new Date(toady.getFullYear(), 5, 11);
@@ -96,115 +95,118 @@ document.addEventListener("DOMContentLoaded", function() {
     })
 
     inputs[5].addEventListener('input', (event) => {
-    const passcode = Array.from(inputs).map(input => input.value).join('');
+        const passcode = Array.from(inputs).map(input => input.value).join('');
 
-    
-    if (passcode === '352636') {
-//--------------------------------------------------------------------------------
+        const digits = passcode.split('').map(Number);
+        const sumall = digits.reduce((acc, curr) => acc + curr, 0);
 
-        Lastinput.blur();
+        
+        if (sumall === 25) {
+    //--------------------------------------------------------------------------------
 
-        imgloaded();
-
-        Notice.style.opacity = 1;
-
+                Lastinput.blur();
 
 
-        Access.addEventListener('click', function() {
+                window.scrollTo({
+                    top:0,
+                    behaviour: 'auto'
+                })
 
-//--------------------------------------------------------------------------------
-
-            window.scrollTo({
-                top:0,
-                behaviour: 'auto'
-            })
-
-            document.body.style.overflowY = "auto";
+                document.body.style.overflowY = "auto";
 
 
-            setTimeout(function() {
-                welcomeText.style.opacity = "0";
-                welcomeText.style.transform = "translateY(-30px)";
-                welcomeText.style.zIndex = '0';
+                setTimeout(function() {
+                    welcomeText.style.opacity = "0";
+                    welcomeText.style.transform = "translateY(-30px)";
+                    welcomeText.style.zIndex = '0';
 
-            }, 100);
+                }, 100);
 
-            setTimeout(function() {
-                main.style.filter = "blur(0px)";
-                main.style.opacity = 1;
-                Header.style.transform = "translateY(-0px)";
-                Box.forEach(box => {    
-                    box.style.transform = "translateY(-0px)";
-                });
-                Above.forEach(above => {
-                    above.style.transform = "translateY(-0px)";
-                });
-            }, 300);
+                setTimeout(function() {
+                    main.style.filter = "blur(0px)";
+                    main.style.opacity = 1;
+                    Header.style.transform = "translateY(-0px)";
+                    Box.forEach(box => {    
+                        box.style.transform = "translateY(-0px)";
+                    });
+                    Above.forEach(above => {
+                        above.style.transform = "translateY(-0px)";
+                    });
+                }, 300);
+              
 
-            Salert.addEventListener('click', function() {
-                window.scrollBy(0, 1);
-            });
-
-            setTimeout(function() {
-                Salert.style.opacity = 0.5;
-            }, 600);
-
-            var orgheight = getComputedStyle(Filler).height;
-
-            setTimeout(function() {
-                window.addEventListener("scroll", function() {
-                    const scroll=window.scrollY > 0;
-                    Header.classList.toggle("scrolled", scroll);
-                    if (scroll) {
-                        Header.style.transform = "translateY(-70px)"
-                        Header.style.opacity = 0;
-                        Header.style.filter = "blur(20px)";
-                        setTimeout(function() {
-                            Header.style.filter = "blur(0px)";                 
-                        },200);
-                        Header.style.transition = "opacity 0.2s ease-in-out, transform 0.7s cubic-bezier(0.210, 0.000, 0.000, 1.000), filter 0.4s ease-in-out";
-                        Ctop.style.opacity = 1;
-                        Filler.style.height = "60px";
-                        Box2.style.scale = 1;
-                        Box2.style.filter = "blur(0px)";
-                        Box2.style.opacity = 1;
+                if (passcode === '352636'){
+                    imgloaded();
+                    setTimeout(function() {
+                        Salert.style.opacity = 0.5;
+                    }, 600);
+                    Salert.addEventListener('click', function() {
+                        window.scrollBy(0, 1);
                         Salert.style.opacity = 0;
                         Salert.style.zIndex = 0;
                         Salert.style.filter = "blur(10px)";
-                    }
-                    else {
-                        Header.style.transform = "translateY(0px)"
-                        setTimeout(function() {
-                            Header.style.opacity = 1;                    
-                        },200);
-                        Ctop.style.opacity = 0;
-                        Filler.style.height = orgheight;
-                        this.setTimeout(function() {
-                            Box2.style.filter = "blur(20px)";
-                            Box2.style.opacity = 0.3;
-                            Box2.style.scale = 0.95;
-                        },300)
-                        
-                    }
+                        window.addEventListener("scroll", function() {
+                            const scroll=window.scrollY > 0;
+                            Header.classList.toggle("scrolled", scroll);
+                            if (scroll) {
+                                Box2.style.scale = 1;
+                                Box2.style.filter = "blur(0px)";
+                                Box2.style.opacity = 1;
+                            }
+                            else {
+                                this.setTimeout(function() {
+                                    Box2.style.filter = "blur(20px)";
+                                    Box2.style.opacity = 0.3;
+                                    Box2.style.scale = 0.95;
+                                },300)                     
+                            }
+                        });
+                    });
+                };
+
+                var orgheight = getComputedStyle(Filler).height;
+
+                setTimeout(function() {
+                    window.addEventListener("scroll", function() {
+                        const scroll=window.scrollY > 0;
+                        Header.classList.toggle("scrolled", scroll);
+                        if (scroll) {
+                            Header.style.transform = "translateY(-70px)"
+                            Header.style.opacity = 0;
+                            Header.style.filter = "blur(20px)";
+                            setTimeout(function() {
+                                Header.style.filter = "blur(0px)";                 
+                            },200);
+                            Header.style.transition = "opacity 0.2s ease-in-out, transform 0.7s cubic-bezier(0.210, 0.000, 0.000, 1.000), filter 0.4s ease-in-out";
+                            Ctop.style.opacity = 1;
+                            Filler.style.height = "60px";
+                        }
+                        else {
+                            Header.style.transform = "translateY(0px)"
+                            setTimeout(function() {
+                                Header.style.opacity = 1;                    
+                            },200);
+                            Ctop.style.opacity = 0;
+                            Filler.style.height = orgheight;                       
+                        }
+                    });
+                }, 300);
+
+
+    //--------------------------------------------------------------------------------
+        } else {
+                inputs.forEach((input, index) => {
+                input.value = '';
+                input.style.borderColor = 'var(--notice-color)';
+                if (index === 0) {
+                    input.focus();
+                }
+                setTimeout(() => {
+                    input.style.borderColor = 'var(--shadow-colorsubtle)';
+                }, 150);
                 });
-            }, 300);
-
-        });
-
-//--------------------------------------------------------------------------------
-    } else {
-        Notice.style.opacity = 0;
-        inputs.forEach((input, index) => {
-        input.value = '';
-        input.style.borderColor = 'var(--notice-color)';
-        if (index === 0) {
-            input.focus();
         }
-        setTimeout(() => {
-            input.style.borderColor = 'var(--shadow-colorsubtle)';
-        }, 150);
-        });
-    }
+
     });
 
 //-------------------------------------------------------------------------------
